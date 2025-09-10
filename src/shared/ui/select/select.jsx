7575187dabe5f10807a7215df4414bd72ae9fb18
ikromovvv <isardor859@gmail.com>
2025-09-10 17:17:27 +0300

    fds

--- src/shared/ui/select/select.jsx
+++ src/shared/ui/select/select.jsx
@@ -55,7 +55,7 @@ export const Select = React.memo(({
     const renderOptionsOfSelect = useCallback(() => {
         return optionsData?.map((item, index) => {
             const value = item[keyValue] || item?.id || item?.value || item?.name || item;
-            const key = item?.name || item?.number || item?.old_id || item?.days || item.num || item?.user && `${item?.user?.name} ${item.user?.surname}` || item?.branch || item[keyValue] || item;
+            const key = item?.name || item?.number || item?.old_id || item?.days || item.num || item?.user && `${item?.user?.name} ${item.user?.surname}` || item?.branch || item[keyValue] || `${item?.start_date} - ${item.end_date}`  ||  item;
 
             return (
                 <option
